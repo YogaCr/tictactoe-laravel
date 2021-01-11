8aeadefe71eef3dd9848e81678495f916f6a6c2a
Yoga Cahya Romadhon <yogacahya08@gmail.com>
2021-01-11 17:36:00 +0700

    Add schedule task for remove old matches

---
+++ app/Console/Commands/DeleteOldMatch.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Match;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class DeleteOldMatch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'match:delete';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete old matches';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Match::where('updated_at','<',Carbon::now()->subMinutes(3))->delete();
+    }
+}
